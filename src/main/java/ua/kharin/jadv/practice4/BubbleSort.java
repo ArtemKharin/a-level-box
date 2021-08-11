f1cf4a95508ef0ec65a78d7cb6288e475cefe0f9
Artem Kharin <artem.kharin96@gmail.com>
2021-08-11 12:13:09 +0300

    Add lesson 4 example for java adv groups

---
+++ src/main/java/ua/kharin/jadv/practice4/BubbleSort.java
@@ -0,0 +1,34 @@
+package ua.kharin.jadv.practice4;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] unsorted = {3, 5, 6, 7, 2, -5, 0, 222, -33, 1, 0, 2, 6, 33, 77, 2, 77};
+        System.out.println(Arrays.toString(unsorted));
+        int[] sorted = arrayCopy(unsorted);
+        sort(sorted);
+        System.out.println(Arrays.toString(sorted));
+    }
+
+    public static void sort(int[] array) {
+        boolean isChanged;
+        int changeAmount = 0;
+        do {
+            isChanged = false;
+            for (int i = 0; i < array.length - 1 - changeAmount; i++) {
+                if (array[i] > array[i + 1]) {
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    isChanged = true;
+                }
+            }
+            changeAmount++;
+        } while (isChanged);
+    }
+
+    public static int[] arrayCopy(int[] initialArray) {
+        return Arrays.copyOf(initialArray, initialArray.length);
+    }
+}
