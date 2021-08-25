bc0de5f583224bb41ff433cd1d60d7e5b1e90366
Artem Kharin <artem.kharin96@gmail.com>
2021-08-25 16:24:16 +0300

    Add lesson 6 example for java adv groups

---
+++ src/main/java/ua/kharin/jadv/practice6/IntegerBox.java
@@ -0,0 +1,48 @@
+package ua.kharin.jadv.practice6;
+
+/*Создать класс IntegerBox, который содержит в себе:
+        •	int поле value
+        •	конструктор с int параметром
+        •	метод toString который возвращает значение value
+        •	метод void increase(int value)
+        •	метод void decrease(int value)
+
+        в main вызвать конструктор,
+        вывести в консоль значение переменной IntegerBox,
+        вызвать метод increase(2),
+        вывести в консоль,
+        вызвать метод decrease(3),
+        вывести в консоль
+*/
+
+public class IntegerBox {
+    private int value;
+
+    public IntegerBox(int value) {
+        this.value = value;
+    }
+
+    public static void main(String[] args) {
+        IntegerBox box = new IntegerBox(5);
+        System.out.println(box);
+        box.increase(2);
+        System.out.println(box);
+        box.decrease(3);
+        System.out.println(box);
+    }
+
+    public void increase(int value) {
+        this.value += value;
+    }
+
+    public void decrease(int value) {
+        this.value -= value;
+    }
+
+    @Override
+    public String toString() {
+        return "IntegerBox{" +
+                "value=" + value +
+                '}';
+    }
+}
