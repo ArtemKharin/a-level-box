e813c8b57f8027fcd6549570db4116f263554a45
Artem Kharin <artem.kharin96@gmail.com>
2021-10-08 17:51:54 +0300

    [WIP] jwt auth

--- spring-demo/src/main/java/ua/kharin/springdemo/configuration/SecurityConfig.java
+++ spring-demo/src/main/java/ua/kharin/springdemo/configuration/SecurityConfig.java
@@ -2,29 +2,36 @@ package ua.kharin.springdemo.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import ua.kharin.springdemo.security.JWTAuthenticationFilter;
+import ua.kharin.springdemo.security.JWTAuthorizationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        http.cors().and()
                 .authorizeRequests()
                 .antMatchers("/users").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin()
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
+                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+                .addFilter(new JWTAuthorizationFilter(authenticationManager()))
+                // this disables session creation on Spring Security
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Bean
@@ -32,11 +39,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public UserDetailsService userDetailsService() {
         UserDetails user =
                 User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
+                        .username("1")
+                        .password("1")
                         .roles("USER")
                         .build();
 
         return new InMemoryUserDetailsManager(user);
     }
+
+    /*@Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService());
+    }*/
 }
---
+++ spring-demo/src/main/java/ua/kharin/springdemo/security/JWTAuthorizationFilter.java
@@ -0,0 +1,71 @@
+package ua.kharin.springdemo.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import lombok.SneakyThrows;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class JWTAuthorizationFilter extends AbstractAuthenticationProcessingFilter {
+
+    public JWTAuthorizationFilter(AuthenticationManager authManager) {
+        super(authManager);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req,
+                                    HttpServletResponse res,
+                                    FilterChain chain) throws IOException, ServletException {
+
+    }
+
+    // Reads the JWT from the Authorization header, and then uses JWT to validate the token
+    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+
+        if (token != null) {
+            // parse the token.
+            String user = JWT.require(Algorithm.HMAC512("SECRET_KEY".getBytes()))
+                    .build()
+                    .verify(token.replace("Bearer ", ""))
+                    .getSubject();
+
+            if (user != null) {
+                // new arraylist means authorities
+                return new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
+            }
+
+            return null;
+        }
+
+        return null;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+        String header = request.getHeader("Authorization");
+
+        if (header == null || !header.startsWith("Bearer ")) {
+            throw new AuthenticationCredentialsNotFoundException("No JWT token found in request headers");
+        }
+
+        String authToken = header.substring(7);
+
+        JwtAuthenticationToken authRequest = new JwtAuthenticationToken(authToken);
+
+        return getAuthenticationManager().authenticate(authRequest);
+    }
+}
