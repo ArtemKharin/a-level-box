53d4def076425a5266b73fe91978ab52f4cb2915
Artem Kharin <artem.kharin96@gmail.com>
2023-04-05 20:58:35 +0300

    11111

--- basics/src/main/java/ua/kharin/jadv/arrays/Task3.java
+++ basics/src/main/java/ua/kharin/jadv/arrays/Task3.java
@@ -7,6 +7,7 @@ public class Task3 {
     public static void main(String[] args) {
         double[] prices = {1234, 100.4, 222.22, 1111,
                 111, 15.9, 20000.99, 1999.99,
+                111, 15.9, 20000.99, 1999.99,
                 1111, 15.4, 1000.99, 19999.99,
                 12.50, 66.3, 650_000, 650001};
         System.out.println(calcSumConditionally(prices));
